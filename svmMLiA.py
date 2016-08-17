8d8fad7f370f3d196523adb66e28e66dcfa8eee9
Computer-Room <longtolin@foxmail.com>
2016-08-17 12:15:54 +0800

    Add loadDataSet() selectJrand()  clipAlpha() functions to svmMLiA.py

---
+++ svmMLiA.py
@@ -0,0 +1,39 @@
+
+def loadDataSet(filename):
+    '''
+
+    :param filename:
+    :return:
+    '''
+    dataMat = []; labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = line.strip().split('\t')
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+
+def selectJrand(i, m):
+    '''
+
+    :param i:
+    :param m:
+    :return:
+    '''
+    j = i
+    while (j==i):
+        j = int(random.uniform(0, m))
+    return  j
+
+def clipAlpha(aj, H, L):
+    '''
+
+    :param aj:
+    :param H:
+    :param L:
+    :return:
+    '''
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
