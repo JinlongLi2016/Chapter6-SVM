6243c4ad26429d4070fd37a0fbd5cb2ef7813da8
Computer-Room <longtolin@foxmail.com>
2016-08-17 12:21:05 +0800

    Fix the loadDataSet() founction

--- svmMLiA.py
+++ svmMLiA.py
@@ -12,6 +12,7 @@ def loadDataSet(filename):
         lineArr = line.strip().split('\t')
         dataMat.append([float(lineArr[0]), float(lineArr[1])])
         labelMat.append(float(lineArr[2]))
+    return dataMat, labelMat
 
 def selectJrand(i, m):
     '''
