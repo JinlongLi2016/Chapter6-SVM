ceb672be8b3ddecc8509a4f78d359ca13ddd87f0
Computer-Room <longtolin@foxmail.com>
2016-08-29 18:42:24 +0800

    full SMO algorithm, 1 stage.

--- svmMLiA.py
+++ svmMLiA.py
@@ -54,7 +54,7 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
     b = 0; m,n = shape(dataMatrix)
     alphas = mat(zeros((m, 1)))
     iter = 0
-    while (iter<maxIter):
+    while iter<maxIter:
         alphaPairsChanged = 0
         for i in range(m):
             fXi = float(multiply(alphas, labelMat).T*
@@ -104,3 +104,36 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
         else: iter = 0
         print("iteration number: %d"%iter)
     return b, alphas
+
+class optStruct:
+    def __init__(self, dataMatIn, classLabels, C, toler):
+        self.X = dataMatIn
+        self.labelMat = classLabels
+        self.C = C
+        self.m = shape(dataMatIn)[0]
+        self.alphas = mat(zeros((self.m, 1)))
+        self.b = 0
+        self.eCache = mat(zeros((self.m, 2)))
+
+def calcEk(oS, k):
+        fXk = float(multiply(oS.alphas, oS.labelMat).T*\
+                    (oS.X*oS.X[k, :])) + oS.b
+        Ek = fXk - float(oS.labelMat[k])
+        return Ek
+
+def selectJ(i, oS, Ei):
+    maxK = -1; maxDeltaE = 0; Ej = 0
+    oS.eCache[i] = [1, Ei]
+    validEcacheList = nonzero(oS.eCache[:,0].A)[0]
+    if len(validEcacheList)>1:
+        for k in validEcacheList:
+            if k == i: continue
+            Ek = calcEk(oS, k)
+            deltaE = abs(Ei - Ek)
+            if deltaE > maxDeltaE:
+                maxK = k; maxDeltaE = deltaE;Ej = Ek
+        return maxK, Ej
+    else:
+        j = selectJrand(i, oS.m)
+        Ej = calcEk(oS, j)
+    return j, Ej
